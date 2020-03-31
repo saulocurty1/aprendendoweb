71ffeeaa88ce475797be2c5e78645939437cf560
Saulo Curty <48524825+saulocurty1@users.noreply.github.com>
2020-03-31 01:21:24 -0300

    Add files via upload
    Versao Com a primeira parte do HTML completa

---
+++ scripts.js
@@ -0,0 +1,18 @@
+document
+    .querySelector("button.fat")
+    .addEventListener("click", function(){
+        document
+            .querySelector("footer")
+            .classList
+            .toggle("hide")
+    })
+    
+/*document
+    .querySelector("button.fat")
+    .addEventListener("click", function(){
+        document
+            .querySelector("#footer2")
+            .classList
+            .toggle("hide")
+    })
+*/
